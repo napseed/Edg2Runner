2b174ed39555348449ca2cfc1c4e3825954614a9
napseed <81262310+napseed@users.noreply.github.com>
2025-05-01 00:31:40 +0900

    feat : 플레이어 무기 구조를 건드리는 중입니다

---
+++ Assets/Resources/Scripts/Player/PlayerBullet.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour
+{
+    [SerializeField]
+    protected float bulletSpeed = 0.0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Resources/Scripts/Player/PlayerWeapon.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+
+/// <summary>
+/// 플레이어가 달 무기 클래스
+/// 여러 무기들은 이 클래스를 상속받아서 사용하자
+/// </summary>
+public class PlayerWeapon : MonoBehaviour
+{
+    [SerializeField]
+    protected GameObject bullet;
+    [SerializeField]
+    protected float fireTerm;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
